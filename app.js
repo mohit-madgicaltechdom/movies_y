5e7ec9f0fd0e92cf6c4554ed7f3bb6d129ce02c3
mohit-madgicaltechdom.com <mohit.jha@madgicaltechdom.com>
2022-03-09 03:47:54 +0530

    added logo

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ arrows.forEach((arrow, i) => {
     const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      movieLists[i].style.transform = `translateX(${
-        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
-      }px)`;
+      movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+        }px)`;
     } else {
       movieLists[i].style.transform = "translateX(0)";
       clickCounter = 0;
@@ -33,3 +32,12 @@ ball.addEventListener("click", () => {
   });
   ball.classList.toggle("active");
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
